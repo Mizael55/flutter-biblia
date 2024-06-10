b0e3a86129f2c21259519ea2b81354fd527f7fd5
Mizael Soler <misaellsoler@gmail.comclear>
2024-06-10 15:04:17 -0400

    adding navigation_bar

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -19,86 +19,104 @@ class HomeScreen extends StatelessWidget {
       );
     }
     return Scaffold(
-        appBar: AppBar(
-          leading: Builder(
-            builder: (context) => IconButton(
-                icon: const Icon(Icons.menu, color: Colors.black),
-                onPressed: () {
-                  Scaffold.of(context).openDrawer();
-                }),
-          ),
-          centerTitle: true,
-          title: Text('${chapter[0]['Book']}',
-              style: const TextStyle(
-                  color: Colors.black,
-                  fontSize: 22.0,
-                  fontWeight: FontWeight.bold)),
-          
-          actions: <Widget>[
-            IconButton(
-              icon: const Icon(Icons.music_note),
+      appBar: AppBar(
+        leading: Builder(
+          builder: (context) => IconButton(
+              icon: const Icon(Icons.menu, color: Colors.black),
               onPressed: () {
-                Navigator.push(
-                    context,
-                    MaterialPageRoute(
-                        builder: (context) => const LiricsSongsScreen(
-                              coro: true,
-                            )));
-              },
-            ),
-            // agrega otro icono tipo himnario a la derecha del appbar
-            IconButton(
-              icon: const Icon(Icons.book),
-              onPressed: () {
-                Navigator.push(
-                    context,
-                    MaterialPageRoute(
-                        builder: (context) => const LiricsSongsScreen(
-                              coro: false,
-                            )));
-              },
-            ),
-          ],
+                Scaffold.of(context).openDrawer();
+              }),
         ),
-        drawer: const DrawerScreen(),
-        body: ListView.builder(
-          itemCount: chapter.length,
-          itemBuilder: (context, index) {
-            final data = chapter[index];
-            return ListTile(
-              title: data['Verse'] == 1
-                  ? Text(
-                      'Capitulo: ${data['Chapter']}',
-                      style: const TextStyle(
-                        fontSize: 20.0,
-                        fontWeight: FontWeight.bold,
-                      ),
-                    )
-                  : null,
-              subtitle: Padding(
-                padding: const EdgeInsets.all(8.0),
-                child: Row(
-                  crossAxisAlignment:
-                      CrossAxisAlignment.start, // Alinea los widgets al inicio
-                  children: <Widget>[
-                    Text(
-                      '${data['Verse']}  ',
-                      style: const TextStyle(
-                        fontSize: 18.0,
-                        fontWeight: FontWeight.bold,
-                      ),
+        centerTitle: true,
+        title: Text('${chapter[0]['Book']}',
+            style: const TextStyle(
+                color: Colors.black,
+                fontSize: 22.0,
+                fontWeight: FontWeight.bold)),
+        actions: <Widget>[
+          IconButton(
+            icon: const Icon(Icons.music_note),
+            onPressed: () {
+              Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                      builder: (context) => const LiricsSongsScreen(
+                            coro: true,
+                          )));
+            },
+          ),
+          // agrega otro icono tipo himnario a la derecha del appbar
+          IconButton(
+            icon: const Icon(Icons.book),
+            onPressed: () {
+              Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                      builder: (context) => const LiricsSongsScreen(
+                            coro: false,
+                          )));
+            },
+          ),
+        ],
+      ),
+      drawer: const DrawerScreen(),
+      body: ListView.builder(
+        itemCount: chapter.length,
+        itemBuilder: (context, index) {
+          final data = chapter[index];
+          return ListTile(
+            title: data['Verse'] == 1
+                ? Text(
+                    'Capitulo: ${data['Chapter']}',
+                    style: const TextStyle(
+                      fontSize: 20.0,
+                      fontWeight: FontWeight.bold,
                     ),
-                    Expanded(
-                      child: Text(
-                        '${data['Text']}',
-                        style: const TextStyle(fontSize: 16.0),
-                      ),
+                  )
+                : null,
+            subtitle: Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Row(
+                crossAxisAlignment:
+                    CrossAxisAlignment.start, // Alinea los widgets al inicio
+                children: <Widget>[
+                  Text(
+                    '${data['Verse']}  ',
+                    style: const TextStyle(
+                      fontSize: 18.0,
+                      fontWeight: FontWeight.bold,
                     ),
-                  ],
-                ),
+                  ),
+                  Expanded(
+                    child: Text(
+                      '${data['Text']}',
+                      style: const TextStyle(fontSize: 16.0),
+                    ),
+                  ),
+                ],
               ),
-            );
-          },
-        ));
+            ),
+          );
+        },
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        backgroundColor: Colors.blue[200],
+        items: const <BottomNavigationBarItem>[
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home, color: Colors.black),
+            label: 'Inicio',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.search, color: Colors.black),
+            label: 'Buscar',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.settings, color: Colors.black),
+            label: 'Ajustes',
+          ),
+        ],
+      )
+      
+    );
   }
 }
